8b1786f8a0c630294367ed3f79f0546dc03b0fff
Miriam Cortes <mcortes.umiami@gmail.com>
2016-08-24 16:31:24 -0700

    Bank Account | Wave 2 requirements

--- bank_account.rb
+++ bank_account.rb
@@ -1,3 +1,4 @@
+### WAVE 1 ###
 # Create a Bank module which will contain your Account class and any future bank account logic.
 # Create an Account class which should have the following functionality:
  # A new account should be created with an ID and an initial balance
@@ -8,29 +9,63 @@
   # A new account cannot be created with initial negative balance - this will raise an ArgumentError
   # The withdraw method does not allow the account to go negative - Will output a warning message and return the original un-modified balance
 
-## Optional: ##
-  # Create an Owner class which will store information about those who own the Accounts.
-    # should have info like name and address and any other identifying information that an account owner would have.
-  # Add an owner property to each Account to track information about who owns the account.
-  # The Account can be created with an owner, OR you can create a method that will add the owner after the Account has already been created.
+### WAVE 2 ###
+# Update the Account class to be able to handle all of these fields from the CSV file used as input.
+  # For example, manually choose the data from the first line of the CSV file and ensure you can create a new instance of your Account using that data
+# Add the following class methods to your existing Account class
+  # self.all - returns a collection of Account instances, representing all of the Accounts described in the CSV. See below for the CSV file specifications
+  # self.find(id) - returns an instance of Account where the value of the id field in the CSV matches the passed parameter
+# CSV Data File
+# Bank::Account
+# The data, in order in the CSV, consists of:
+  # ID - (Fixnum) a unique identifier for that Account
+  # Balance - (Fixnum) the account balance amount, in cents (i.e., 150 would be $1.50)
+  # OpenDate - (Datetime) when the account was opened
 
-
+require 'csv'
+require 'awesome_print'
 
 module Bank
   class Account
-    attr_accessor :initial_balance, :deposit_amount, :withdraw_amount
-    def initialize(initial_balance)
-      @id = Random.rand(100000..999999)
+    attr_accessor :balance, :deposit_amount, :withdraw_amount, :accountnumber, :account_open_date
+    @@account_hash = {}
+    def initialize(balance,account_open_date = "today")
+      # @id = Random.rand(100000..999999)
       @account_owner
-      if initial_balance <= 0
+      @balance
+      @account_open_date = account_open_date
+
+      if balance <= 0
         raise ArgumentError.new("You can't open an account with $0 or less.")
       else
-        @balance = initial_balance
+        @balance = balance
       end
-      puts "Thanks for opening an account with Mr. T!"
-      puts "Account number: #{@id}"
+      # puts "Thanks for opening an account with Mr. T!"
+      # puts "Account number: #{@id}"
       show_current_balance
-      puts "Thanks for giving me your money, Fool!"
+      return "Thanks for giving me your money, Fool!"
+    end
+
+    def self.reading_accounts
+      csv = CSV.read("support/accounts.csv")
+      csv.each do |account|
+        key = account[0]
+        balance = account[1].to_f / 100
+        account_open_date = account[2]
+        @@account_hash[key] = self.new(balance,account_open_date)
+        # return @@account_hash
+      end
+    end
+
+    def self.all
+      @@account_hash.each do |key,value|
+        puts "#{key} has balance $#{value.balance} & opened #{value.account_open_date[0..9]}"
+      end
+      return @@account_hash
+    end
+
+    def self.find(id)
+      return @@account_hash[id.to_s]
     end
 
     def deposit(deposit_amount)
@@ -56,24 +91,62 @@ module Bank
     end
 
     def show_current_balance
-      puts "Balance: $#{sprintf "%.2f", @balance}"
+      return "Balance: $#{sprintf "%.2f", @balance}"
     end
   end
 
+  ## WAVE 1 OPTIONAL...DIDN'T HAPPEN ##
+  # class Owner
+  #   attr_accessor :name, :city, :state, :zip
+  #   def initialize(user_details_hash)
+  #     @name = user_details_hash[:name]
+  #     @city = user_details_hash[:city]
+  #     @state = user_details_hash[:state]
+  #     @zip = user_details_hash[:zip]
+  #     @user_id
+  #     @user_balance
+  #     puts "You've got a user!"
+  #   end
+  #
+  #   def add_account_info_to_owner()
+  #     @user_balance = Bank::Account::balance
+  #     @user_id = Bank::Account::id
+  #   end
+  #
+  # end
+
 end
 
+# puts "..........Opening a new account with $1.57 balance results in.........."
+# account1 = Bank::Account.new(1.57)
+# puts "..........a deposit of $3.........."
+# puts account1.deposit(3)
+# puts "..........Trying to withdraw $8.........."
+# puts account1.withdraw(8)
+# puts "..........Showing current balance.........."
+# puts account1.show_current_balance
+# puts "..........Depositing a negative.........."
+# puts account1.deposit(-3)
+# puts "..........Trying to withdraw $0.........."
+# puts account1.withdraw(0)
+# puts "..........Opening a new account with $0 balance results in.........."
+# puts account200 = Bank::Account.new(0)
 
-puts "..........Opening a new account with $1.57 balance results in.........."
-account1 = Bank::Account.new(1.57)
-puts "..........a deposit of $3.........."
-account1.deposit(3)
-puts "..........Trying to withdraw $8.........."
-account1.withdraw(8)
-puts "..........Showing current balance.........."
-account1.show_current_balance
-puts "..........Depositing a negative.........."
-account1.deposit(-3)
-puts "..........Trying to withdraw $0.........."
-account1.withdraw(0)
-puts "..........Opening a new account with $0 balance results in.........."
-account200 = Bank::Account.new(0)
+puts ".........Reading the csv file and putting it into a hash........."
+users = Bank::Account.reading_accounts
+ap users
+
+puts ".........Printing out all the accounts' information........."
+all_users = Bank::Account.all
+ap all_users
+
+puts ".........Printing out one accounts' information........."
+one_new_user = Bank::Account.find(15153)
+ap one_new_user
+
+## MY FAILED ATTEMPT AT DOING THE WAVE 1 OPTIONALS ##
+# user2_info = Bank::Owner.new(name:"name",city:"city",state:"state",zip:"zip")
+# puts "User 2's info: #{user2_info}"
+# user2_account = Bank::Account.new(5.67)
+# user2_info.add_account_info_to_owner
+##But how do I get the balance
